a07fc9b64c8246f79b8eabca49883174a0e64010
Denis Karnafel <denisk9999@mail.ru>
2023-06-29 18:51:12 +0700

    improved random number generator

--- src/randomNum.js
+++ src/randomNum.js
@@ -1,3 +1,3 @@
-const getRandomNumber = (max) => Math.round(Math.random() * max);
+const getRandomNumber = (min = 1, max = 100) => Math.floor(Math.random() * (max - min + 1) + min);
 
 export default getRandomNumber;
