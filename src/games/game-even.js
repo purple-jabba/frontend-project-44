4d2d6cbfe83dc4c2b773592c169212a7a964b371
Denis Karnafel <denisk9999@mail.ru>
2023-06-29 18:51:54 +0700

    update game-even.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,12 +1,12 @@
 import gameEngine from '../index.js';
 import getRandomNumber from '../randomNum.js';
 
-const isEven = (num) => num % 2 === 0;
-
 const ruleEven = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (num) => num % 2 === 0;
+
 const gameEven = () => {
-  const question = getRandomNumber(100);
+  const question = getRandomNumber();
   const answer = isEven(question) ? 'yes' : 'no';
   return [question, answer];
 };
