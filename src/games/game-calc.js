07a56c0fb4dc724e30596147d8aa074dde8ce011
Denis Karnafel <denisk9999@mail.ru>
2023-07-01 13:17:45 +0700

    update game-calc.js

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,35 +1,22 @@
 import startGame from '../index.js';
 import getRandomNumber from '../utils/randomNum.js';
+import calculate from '../utils/calc.js';
 
 const ruleCalc = 'What is the result of the expression?';
 
-const getRandomSymb = () => {
-  const symbols = ['+', '-', '*'];
-  const symbIndex = getRandomNumber(0, symbols.length - 1);
-  return symbols[symbIndex];
-};
-
 const gameCalc = () => {
+  const getRandomSymb = () => {
+    const symbols = ['+', '-', '*'];
+    const symbIndex = getRandomNumber(0, symbols.length - 1);
+    return symbols[symbIndex];
+  };
+
   const numberOne = getRandomNumber(1, 100);
   const numberTwo = getRandomNumber(1, 100);
   const symbol = getRandomSymb();
 
-  let calc = symbol;
-  switch (calc) {
-    case '+':
-      calc = numberOne + numberTwo;
-      break;
-    case '-':
-      calc = numberOne - numberTwo;
-      break;
-    case '*':
-      calc = numberOne * numberTwo;
-      break;
-    default:
-  }
-
   const question = `${numberOne} ${symbol} ${numberTwo}`;
-  const answer = calc.toString();
+  const answer = calculate(numberOne, numberTwo, symbol).toString();
 
   return [question, answer];
 };
