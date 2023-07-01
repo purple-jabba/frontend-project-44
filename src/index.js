33ef92b10df17fc833e91a88b3f7c4cd64afa7d6
Denis Karnafel <denisk9999@mail.ru>
2023-07-01 12:14:27 +0700

    fix index.js

--- src/index.js
+++ src/index.js
@@ -15,13 +15,12 @@ const startGame = (rule, game) => {
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    if (userAnswer.toLowerCase() === correctAnswer) {
-      console.log('Correct!');
-    } else {
+    if (userAnswer.toLowerCase() !== correctAnswer) {
       console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${userName}!`);
 };
