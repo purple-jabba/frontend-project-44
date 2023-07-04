173969a3dace1727daf25659417a448fdea74705
Denis Karnafel <denisk9999@mail.ru>
2023-07-04 14:16:15 +0700

    update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,35 +1,32 @@
 import startGame from '../index.js';
 import getRandomNumber from '../utils/randomNum.js';
-import arrayToString from '../utils/arrToStr.js';
 
 const description = 'What number is missing in the progression?';
 
+const getRandomProgression = (startPoint, step, length) => {
+  const result = [];
+  let temp;
+
+  result.push(startPoint);
+
+  for (let i = 0; i <= length - 1; i += 1) {
+    temp = result[i] + step;
+    result.push(temp);
+  }
+  return result;
+};
+
 const gameProg = () => {
-  const getRandomProg = (startPoint, step, length) => {
-    const result = [];
-    let temp;
-
-    result.push(startPoint);
-
-    for (let i = 0; i <= length - 1; i += 1) {
-      temp = result[i] + step;
-      result.push(temp);
-    }
-    return result;
-  };
-
   const firstNum = getRandomNumber(1, 50);
-  const progStep = getRandomNumber(1, 10);
-  const progLength = getRandomNumber(4, 9);
+  const progressionStep = getRandomNumber(1, 10);
+  const progressionLength = getRandomNumber(4, 9);
 
-  const generatedProg = getRandomProg(firstNum, progStep, progLength);
-  const selectedIndex = getRandomNumber(0, generatedProg.length - 1);
-  const hiddenIndex = [];
-  hiddenIndex.push(...generatedProg);
-  hiddenIndex[selectedIndex] = '..';
+  const generatedProgression = getRandomProgression(firstNum, progressionStep, progressionLength);
+  const selectedIndex = getRandomNumber(0, generatedProgression.length - 1);
+  const answer = generatedProgression[selectedIndex].toString();
 
-  const question = arrayToString(hiddenIndex);
-  const answer = generatedProg[selectedIndex].toString();
+  generatedProgression[selectedIndex] = '..';
+  const question = generatedProgression.join(' ');
 
   return [question, answer];
 };
