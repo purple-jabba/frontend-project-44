0b0128c92f8cf2c1c8664d89305dc80e83d5eb4f
Denis Karnafel <denisk9999@mail.ru>
2023-07-04 14:15:39 +0700

    deleted arrToStr.js - code is no longer used

--- src/utils/arrToStr.js
+++
@@ -1,9 +0,0 @@
-const arrayToString = (array) => {
-  let text = '';
-  for (let i = 0; i <= array.length - 1; i += 1) {
-    text = `${text} ${array[i]}`;
-  }
-  return text.trim();
-};
-
-export default arrayToString;
